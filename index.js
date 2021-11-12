6dc09f8036d4ff65e24f948248ce2bc908c961a6
Bipro <biprobarai18@gmail.com>
2021-11-12 14:52:16 +0600

    first

---
+++ index.js
@@ -0,0 +1,115 @@
+const express = require('express')
+const cors = require('cors');
+const { MongoClient } = require('mongodb');
+const ObjectId = require('mongodb').ObjectId;
+require('dotenv').config();
+const app = express()
+
+const port = process.env.PORT || 5000
+
+
+// middleware
+app.use(cors());
+// Get data from client site
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.v2bif.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+console.log(uri)
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run() {
+
+    try {
+        await client.connect();
+        console.log('database connected');
+        const database = client.db('carShop');
+        const carsCollection = database.collection('cars');
+        const usersCollection = database.collection('users');
+
+        // Get cars API
+
+        app.get('/cars', async (req, res) => {
+            const cursor = carsCollection.find({});
+            const cars = await cursor.toArray();
+            res.send(cars);
+        })
+
+        // get Admin
+        app.get('/users/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email: email };
+            const user = await usersCollection.findOne(query);
+            let usAdmin = false;
+            if (user?.role == 'admin') {
+                isAdmin = true;
+            }
+            res.json({ admin: isAdmin });
+        })
+
+        // Post Car API
+
+        app.post('/cars', async (req, res) => {
+            const car = req.body;
+            console.log('hit the post', car)
+
+            const result = await carsCollection.insertOne(car);
+            res.json(result);
+        })
+
+        // post user API
+
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const result = await usersCollection.insertOne(user)
+            res.json(result);
+        })
+
+        // update
+
+        app.put('/users', async (req, res) => {
+
+            const user = req.body;
+            const filter = { email: user.email };
+            const options = { upsert: true }
+            const updateDoc = { $set: user };
+            const result = await usersCollection.updateOne(filter, updateDoc, options);
+            res.json(result);
+        });
+
+        // admin
+
+        app.put('/users/admin', async (req, res) => {
+            const user = req.body;
+            const filter = { email: user.email };
+            const updateDoc = { $set: { role: 'admin' } };
+            const result = await usersCollection.updateOne(filter, updateDoc);
+            res.json(result)
+        })
+
+        // Delete API
+
+        app.delete('/cars/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await carsCollection.deleteOne(query);
+            console.log('delete', result)
+            res.json(result);
+        })
+
+    }
+    finally {
+        // await client.close()
+    }
+
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
